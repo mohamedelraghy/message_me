c062a2a8211fca60514e35595625e0a896471486
mohamed El-Raghy <elraghy8@gmail.com>
2022-11-23 19:46:06 +0200

    add user resorce

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Mohamed", password: "password")
+User.create(username: "Marwa", password: "password")
+User.create(username: "belal", password: "password")
+User.create(username: "Jasmeen", password: "password")
+User.create(username: "Youseef", password: "password")
