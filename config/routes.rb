57b92c26c1f582ade7e6d417d5351e3456ef2920
mohamed El-Raghy <elraghy8@gmail.com>
2022-11-26 17:33:02 +0200

    sign up a user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   get 'messages/create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -7,6 +9,8 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   post 'message', to: 'messages#create'
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 
   mount ActionCable.server, at: '/cable'
 end
